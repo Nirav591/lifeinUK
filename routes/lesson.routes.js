e293b3b31163e9161db4c60801863c6b14fc0f9b
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-27 17:30:33 +0100

    addeeed

--- routes/lesson.routes.js
+++ routes/lesson.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { addLesson } = require('../controllers/lesson.controller');
-const { isAdmin } = require('../middlewares/authMiddleware');
+const { isAdmin } = require('../middleware/authMiddleware');
 const { body, validationResult } = require('express-validator');
 
 
