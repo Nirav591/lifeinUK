61ce0ec2632a51230a222426bafe4a17ba7596b1
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-27 16:12:28 +0100

    added

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6350;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
