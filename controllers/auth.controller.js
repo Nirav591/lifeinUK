89524331a1398381193dd4fdc32500adc91f4ab6
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-27 16:53:36 +0100

    added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -61,4 +61,14 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = { signup, login };
+const getAllUsers = async (req, res) => {
+    try {
+      const [users] = await pool.query('SELECT id, name, email, role, created_at FROM users');
+      res.status(200).json(users);
+    } catch (error) {
+      console.error('Get All Users Error:', error);
+      res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+  };
+
+module.exports = { signup, login, getAllUsers };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { signup, login } = require('../controllers/auth.controller');
+const { signup, login , getAllUsers} = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/users', getAllUsers); // <-- New route
+
 
 module.exports = router;
