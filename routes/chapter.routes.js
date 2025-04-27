dd45ad74ae768726779072ae15b8fee8f537267b
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-27 17:09:43 +0100

    added

--- routes/chapter.routes.js
+++ routes/chapter.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { addChapter, getChapters, updateChapter, deleteChapter } = require('../controllers/chapter.controller');
-const { isAdmin } = require('../middlewares/authMiddleware');
+const { isAdmin } = require('../middleware/authMiddleware');
 const { body, validationResult } = require('express-validator');
 const multer = require('multer');
 const path = require('path');
