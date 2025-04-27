5f22f9d8c5ead22b4a280929370e46483e3ecd6e
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-27 17:17:34 +0100

    added

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const isAdmin = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) return res.status(401).json({ message: 'No token provided' });
 
-    const decoded = jwt.verify(token, "key");
+    const decoded = jwt.verify(token, "mysecretkey");
     if (decoded.role !== 'admin') {
       return res.status(403).json({ message: 'Access denied, Admins only' });
     }
